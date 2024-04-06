5757d09e4f72ff6cfdc34cdd54f32dd6b423930c
juna mac <wnsghk6670@naver.com>
2024-04-06 19:14:07 +0900

    FEAT: 프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = "블로그 제목임";
+  let [titleOfPost, setTitleOfPosst] = useState([
+    "남자 코트 추천",
+    "강남 짬뽕 맛집",
+    "잠심 짜장면 맛집"
+  ]);
+  let [likeCount, setLikeCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="black-nav">
+      <h4>
+        블로그임
+      </h4>
+     </div>
+     <div className='list'>
+      <h4>{titleOfPost[0]}
+        <span onClick={() => {setLikeCount(likeCount + 1)}}> 👍 </span> { likeCount }
+      </h4>
+      <p>2024월 04월 06일 발생</p>
+     </div>
+     <div className='list'>
+      <h4>{titleOfPost[1]}</h4>
+      <p>2024월 04월 06일 발생</p>
+     </div>
+     <div className='list'>
+      <h4>{titleOfPost[2]}</h4>
+      <p>2024월 04월 06일 발생</p>
+     </div>
     </div>
   );
 }
-
 export default App;
