c3bef273a7561b4a40d899687e0f3c08245ff4ce
juna bigcraft <wnsghk6670@naver.com>
2024-04-08 16:16:33 +0900

    REFACTOR: map을 통한 반복요소 제거

--- src/App.js
+++ src/App.js
@@ -1,61 +1,88 @@
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-
   let [titleOfPost, setTitleOfPosst] = useState([
     "남자 코트 추천",
     "강남 짬뽕 맛집",
-    "잠심 짜장면 맛집"
+    "잠심 짜장면 맛집",
   ]);
-  let [likeCount, setLikeCount] = useState(0);
+  let [likeCount, setLikeCount] = useState([0, 0, 0]);
+  let [titleIndex, setTitleIndex] = useState(0);
   let [modal, setModal] = useState(false);
+  let changeTitle = () => {
+    let copy = [...titleOfPost];
+    copy[0] = "여자 코트 추천";
+    setTitleOfPosst(copy);
+  };
 
   return (
     <div className="App">
-     <div className="black-nav">
-      <h4>블로그임</h4> <button onClick={() => {
-        let copy = [...titleOfPost];
-        copy[0] = '여자 코트 추천';
-        setTitleOfPosst(copy);
-      }}>첫번째 글 제목 바뀌는 버튼</button>
-     </div>
-     <button onClick={() =>{
-      let arrayCopy = [...titleOfPost];
-      arrayCopy.sort();
-      setTitleOfPosst(arrayCopy);
-     }}>제목 정렬 버튼</button>
-     <div className='list'>
-      <h4>{titleOfPost[0]}
-        <span onClick={() => {setLikeCount(likeCount + 1)}}> 👍 </span> { likeCount }
-      </h4>
-      <p>2024월 04월 06일 발생</p>
-     </div>
-     <div className='list'>
-      <h4>{titleOfPost[1]}</h4>
-      <p>2024월 04월 06일 발생</p>
-     </div>
-     <div className='list'>
-      <h4 onClick={() =>{setModal(!modal)}}>{titleOfPost[2]}</h4>
-      <p>2024월 04월 06일 발생</p>
-     </div>
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <button
+        onClick={() => {
+          changeTitle();
+        }}
+      >
+        첫번째 글 제목 바뀌는 버튼
+      </button>
+      <button
+        onClick={() => {
+          let arrayCopy = [...titleOfPost];
+          arrayCopy.sort();
+          setTitleOfPosst(arrayCopy);
+        }}
+      >
+        제목 정렬 버튼
+      </button>
+      {titleOfPost.map(function (title, i) {
+        return (
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                setTitleIndex(i);
+                setModal(!modal);
+              }}
+            >
+              {titleOfPost[i]}
+              <span
+                onClick={() => {
+                  let countCopy = [...likeCount];
+                  countCopy[i]++;
+                  setLikeCount(countCopy);
+                }}
+              >
+                👍
+              </span>
+              {likeCount[i]}
+            </h4>
+            <p>2024월 04월 06일 발생</p>
+          </div>
+        );
+      })}
 
-     {
-      modal ? <Modal></Modal> : null
-     }
-
+      {modal ? (
+        <Modal
+          titleOfPost={titleOfPost}
+          changeTitle={changeTitle}
+          titleIndex={titleIndex}
+        />
+      ) : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className='modal'>
-     <h2>제목</h2>
-     <p>날짜</p>
-     <p>상세 내용</p>
+    <div className="modal">
+      <h2>{props.titleOfPost[props.titleIndex]}</h2>
+      <p>날짜</p>
+      <p>상세 내용</p>
+      <button onClick={props.changeTitle}>글수정</button>
     </div>
-  )
+  );
 }
 
 export default App;
