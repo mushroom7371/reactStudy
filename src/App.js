6152370581c51ad99e13f5899ed793d507654736
juna bigcraft <wnsghk6670@naver.com>
2024-04-08 14:12:10 +0900

    FEAT: 모달 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
 
-  let post = "블로그 제목임";
   let [titleOfPost, setTitleOfPosst] = useState([
     "남자 코트 추천",
     "강남 짬뽕 맛집",
     "잠심 짜장면 맛집"
   ]);
   let [likeCount, setLikeCount] = useState(0);
+  let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
      <div className="black-nav">
-      <h4>블로그임</h4> <button onClick={() => setTitleOfPosst(["여자 코트 추천", titleOfPost[1], titleOfPost[2]])}>첫번째 글 제목 바뀌는 버튼</button>
+      <h4>블로그임</h4> <button onClick={() => {
+        let copy = [...titleOfPost];
+        copy[0] = '여자 코트 추천';
+        setTitleOfPosst(copy);
+      }}>첫번째 글 제목 바뀌는 버튼</button>
      </div>
+     <button onClick={() =>{
+      let arrayCopy = [...titleOfPost];
+      arrayCopy.sort();
+      setTitleOfPosst(arrayCopy);
+     }}>제목 정렬 버튼</button>
      <div className='list'>
       <h4>{titleOfPost[0]}
         <span onClick={() => {setLikeCount(likeCount + 1)}}> 👍 </span> { likeCount }
@@ -28,10 +36,26 @@ function App() {
       <p>2024월 04월 06일 발생</p>
      </div>
      <div className='list'>
-      <h4>{titleOfPost[2]}</h4>
+      <h4 onClick={() =>{setModal(!modal)}}>{titleOfPost[2]}</h4>
       <p>2024월 04월 06일 발생</p>
      </div>
+
+     {
+      modal ? <Modal></Modal> : null
+     }
+
     </div>
   );
 }
+
+function Modal() {
+  return (
+    <div className='modal'>
+     <h2>제목</h2>
+     <p>날짜</p>
+     <p>상세 내용</p>
+    </div>
+  )
+}
+
 export default App;
