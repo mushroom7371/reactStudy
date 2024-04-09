67f5e157e203a00efc06bd6bdee4b5363d5b7c5c
juna bigcraft <wnsghk6670@naver.com>
2024-04-09 16:40:00 +0900

    FEAT: 이벤트, 어바웃 페이지 초안 작성 및 라우터 적용

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ import shoes2 from "./img/shoes2.jpg";
 import shoes3 from "./img/shoes3.jpg";
 import { useState } from "react";
 import data from "./data";
-import { Route, Link, Routes } from "react-router-dom";
-import DetailPage from "./DetailPage";
+import { Route, Link, Routes, useNavigate, Outlet } from "react-router-dom";
+import DetailPage from "./pages/DetailPage";
 
 function App() {
   let [shoseData, setShoseData] = useState(data);
   let [shoseImage, setShoseImage] = useState([shoes1, shoes2, shoes3]);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
@@ -19,9 +20,34 @@ function App() {
         <Container style={{ marginLeft: "20px" }}>
           <Navbar.Brand href="/">Shop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
-            <Nav.Link href="#MyPage">MyPage</Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/detail");
+              }}
+            >
+              Cart
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/event");
+              }}
+            >
+              Event
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => {
+                navigate("/about");
+              }}
+            >
+              About
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -49,7 +75,16 @@ function App() {
             </>
           }
         />
-        <Route path="/detail" element={DetailPage()} />
+        <Route path="/detail" element={<DetailPage />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path="two" element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path="*" element={<div>404</div>} />
       </Routes>
     </div>
   );
@@ -62,6 +97,24 @@ function ShopComponent(props) {
       <h4>{props.shoseData[props.i].title}</h4>
       <p>{props.shoseData[props.i].price}</p>
     </Col>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
   );
 }
 
